d1981dc09c90164e954601f8cd4a9ab5a48cea2a
Priyanshu Rajput <priyanshurajput1386@gmail.com>
2025-02-04 09:33:21 +0530

    utils discussion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ import connectDB from "./db/index.js"
 
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is runnign at port : ${process.env.PORT}`)
+        // ,
+        
+        // app.on("error",(error)=>{
+        //     console.log("error:",error);
+        //     throw error;
+        // })
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err);
+})
 
 /*
 import express from "express"
